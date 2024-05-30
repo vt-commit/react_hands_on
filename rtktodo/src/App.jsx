cad2a2a58364c4fcff1ccb24cb0087ab83fa4732
vrushaliTambat <vrushalitambat32@gmail.com>
2024-05-30 10:57:40 -0400

    completed rtktodo

--- rtktodo/src/App.jsx
+++ rtktodo/src/App.jsx
@@ -1,12 +1,13 @@
-import { useState } from "react";
 import "./App.css";
+import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todos";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <h1>Learning Redux</h1>
+      <AddTodo />
+      <Todos />
     </>
   );
 }
--- rtktodo/src/features/todo/todoSlice.js
+++ rtktodo/src/features/todo/todoSlice.js
@@ -6,5 +6,25 @@ const initialState = {
 
 export const todoSlice = createSlice({
     name:'todo',
-    initialState
+    initialState,
+    reducers:{
+        addTodo:(state,action)=>{
+            const todo = {
+                id:nanoid(), 
+                text:action.payload.text
+                //text:action.payload
+            }
+            state.todos.push(todo)
+        },
+        removeTodo:(state,action)=>{
+            state.todos = state.todos.filter((eachTodo)=> action.payload !== eachTodo.id)
+        },
+        // updateTodo:(state,action)=>{
+        //     state.todos.map((eachTodo)=> eachTodo.id === action.payload ? eachTodo.text = action.payload : eachTodo.text)
+        // }
+    }
 })
+
+export const {addTodo,removeTodo} = todoSlice.actions 
+
+export default todoSlice.reducer
