9a16f769eca15522715454ef8e14a5db8f86d545
vrushaliTambat <vrushalitambat32@gmail.com>
2024-06-01 16:27:16 -0400

    service for db and storage

--- blog-website/src/appwrite/config.js
+++ blog-website/src/appwrite/config.js
@@ -73,7 +73,58 @@ export class Service{
 
    //as we don't want staus inactive docs, we will query the db and not get all docs.
    //for active status posts
-   
+   //in case of multiple values give array in place of "active"
+
+   async getAllPost(queries = [Query.equal("status","active")]){
+    try {
+        return await this.databases.listDocuments(
+            conf.appwriteDatabaseId,
+            conf.appwriteCollectionId,
+            queries
+        )
+    } catch (error) {
+        console.log("Appwrite Service :: getAllPost :: error",error)
+        return false;
+    }
+   }
+
+   //storage service
+
+   async uploadFile(file){
+    try {
+        //appwrite returns a public path for the file
+        return await this.bucket.createFile(
+            conf.appwriteBucketId,
+            ID.unique(),
+            file
+        )
+
+    } catch (error) {
+        console.log("Appwrite Service :: uploadFile :: error",error)
+        return false;
+    }
+   }
+
+   async deleteFile(fileId){
+    try {
+        return await this.bucket.deleteFile(
+            conf.appwriteBucketId,
+            fileId
+        )
+
+    } catch (error) {
+        console.log("Appwrite Service :: deleteFile :: error",error)
+        return false;
+    }
+   }
+
+   getFilePreview(fileId){
+    return this.bucket.getFilePreview(
+        conf.appwriteBucketId,
+        fileId
+    ).href
+   }
+
 }
 
 const service =new Service();
