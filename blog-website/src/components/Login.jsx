6cf0aa1eb5a22727ae5d96252f6a10e3a9a1cf24
vrushaliTambat <vrushalitambat32@gmail.com>
2024-06-02 19:45:15 -0400

    Select & PostCard

---
+++ blog-website/src/components/Login.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { login as authLogin } from "../store/authSlice";
+import { Button, Input, Logo } from "./index";
+import { authService } from "../appwrite/auth";
+import { useDispatch } from "react-redux";
+import { useForm } from "react-hook-form";
+
+function Login() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { register, handleSubmit } = useForm();
+  const [error, setError] = useState("");
+
+  const login = async (data) => {
+    setError("");
+    try {
+      const session = await authService.login(data);
+      if (session) {
+        const userData = await authService.getCurrentUser();
+        if (userData) dispatch(authLogin(userData));
+        navigate("/");
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  return <div>Login</div>;
+}
+
+export default Login;
--- blog-website/src/components/index.js
+++ blog-website/src/components/index.js
@@ -3,5 +3,6 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo"
 import LogoutButton from "./Header/LogoutButton"
+import Button from "./Button";
 
-export {Header,Footer, Container,Logo,LogoutButton}
+export {Header,Footer, Container,Logo,LogoutButton,Button}
