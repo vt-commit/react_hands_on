d8fdb822e30ed9803937e3d44d802786f02822e3
vrushaliTambat <vrushalitambat32@gmail.com>
2024-06-05 19:55:31 -0400

    basic handling

--- reactQueryAPIAxios/frontend/src/App.jsx
+++ reactQueryAPIAxios/frontend/src/App.jsx
@@ -1,12 +1,39 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        setLoading(true);
+        setError(false);
+        const response = await axios.get("/api/products");
+        console.log(response.data);
+        setProducts(response.data);
+        setLoading(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    })();
+  }, []);
+
+  if (error) {
+    return <h1>Something went wrong!</h1>;
+  }
+  if (loading) {
+    return <h1>Loading....</h1>;
+  }
 
   return (
     <>
       <h1>API in React</h1>
+      <h2>Number of products are: {products.length}</h2>
     </>
   );
 }
